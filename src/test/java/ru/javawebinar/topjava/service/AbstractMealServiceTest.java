fc3f5208a11fc06c0804bb5d0b01c60e6cfa9bc4
Grigory Kislin <javawebinar@yandex.ru>
2018-08-22 18:34:36 +0300

    8_0_fix

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -80,7 +80,7 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 
     @Test
     void testValidation() throws Exception {
-        Assumptions.assumeTrue(isJpaBased(), "JPA not supported");
+        Assumptions.assumeTrue(isJpaBased(), "Validation not supported (only for JPA)");
         validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "  ", 300), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new Meal(null, null, "Description", 300), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.create(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 9), USER_ID), ConstraintViolationException.class);
