aeb58ead05a43d2b1b423d383b1c7efc1284a6a7
Grigory Kislin <javawebinar@yandex.ru>
2018-09-13 04:40:54 +0300

    11_05_HW10_change_locale

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -68,6 +68,8 @@ $(function () {
         "initComplete": makeEditable
     });
 
+    $.datetimepicker.setLocale(localeCode);
+
 //  http://xdsoft.net/jqplugins/datetimepicker/
     const startDate = $('#startDate');
     const endDate = $('#endDate');
